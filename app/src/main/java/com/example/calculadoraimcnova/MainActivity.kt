c9f7637d3bdfc434bec180972acc1abec162cccb
otavi <otaviogandhi@gmail.com>
2025-07-07 16:36:41 -0300

    Terminando a logica do APP

--- app/src/main/java/com/example/calculadoraimcnova/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimcnova/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +13,8 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnCalcular:Button
+    private lateinit var edtPeso:EditText
+    private lateinit var edtAltura:EditText
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,10 +28,23 @@ class MainActivity : AppCompatActivity() {
 
         }
         btnCalcular = findViewById(R.id.btnCalcular)
+        edtPeso = findViewById(R.id.edtPeso)
+        edtAltura = findViewById(R.id.edtAltura)
 
         btnCalcular.setOnClickListener {
 
             val intent = Intent(this, ResultadoActivity2::class.java)
+
+            val peso = edtPeso.text.toString()
+            val altura = edtAltura.text.toString()
+
+            if (peso.isNotEmpty()&& altura.isNotEmpty()){    // SO EVIARA PARA PROX TELA, SE OS DADOS NAO ESTIVEREM VAZIOS
+
+                intent.putExtra("peso", peso.toDouble())
+                intent.putExtra("altura", altura.toDouble())
+            }
+
+            startActivity(intent)
         }
         }
     }
--- app/src/main/java/com/example/calculadoraimcnova/ResultadoActivity2.kt
+++ app/src/main/java/com/example/calculadoraimcnova/ResultadoActivity2.kt
@@ -1,12 +1,17 @@
 package com.example.calculadoraimcnova
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ResultadoActivity2 : AppCompatActivity() {
+
+    private lateinit var textPeso:TextView
+    private lateinit var textAltura:TextView
+    private lateinit var textResultado:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,32 @@ class ResultadoActivity2 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        textPeso = findViewById(R.id.textPeso)
+        textAltura = findViewById(R.id.textAltura)
+        textResultado = findViewById(R.id.textResultado)
+
+        val bundle = intent.extras //RECUPEANDO OS VALORES DIGITADOS
+
+        if (bundle!=null){//
+
+            val peso = bundle.getDouble("peso")
+            val altura = bundle.getDouble("altura")
+
+            textPeso.text = "Peso informado $peso Kg"
+            textAltura.text = "Peso informado $altura Kg"
+
+            val imc = peso/ (altura * altura)// FAZENDO O CALCULO
+            val resultado =if (imc< 18.5){
+                "Baixo"
+            }else if (imc in 18.5..24.9){
+                "Normal"
+            }else if (imc in 25.0 ..29.9 ){
+                  "Sobrepeso"
+            }else{
+                 "Obesidade"
+            }
+            textResultado.text = resultado
         }
     }
 }
